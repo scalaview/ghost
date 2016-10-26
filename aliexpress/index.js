b13a95b02289fa006709ee251cc8fa76e3224916
scalaview <chailink100@gmail.com>
2016-10-27 01:05:19 +0800

    add onoffline products

--- aliexpress/index.js
+++ aliexpress/index.js
@@ -3,35 +3,56 @@ var async = require("async")
 var config = require("../config")
 var crypto = require('crypto')
 var Promise = require("bluebird")
+var redis = require("redis")
 
-module.exports = function(){
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
+var client = redis.createClient();
+
+module.exports = function(refreshToken, accessToken){
   this.clientKey = config.clientKey
   this.clientSecret = config.clientSecret
+  this.refreshToken = refreshToken
+  this.accessToken = accessToken
+  this.host = "gw.api.alibaba.com"
+  this.suburl = "/openapi/param2"
+  this.version = "1"
+  this.namespace = "aliexpress.open"
+  function init(that){
+    return client.hmgetAsync(["aliexpress:info", "accessToken",
+      "expireTime", "refreshToken", "refreshTokenTimeout"]).then(function(data){
+        that.accessToken = !!that.accessToken ? that.accessToken : data[0]
+        that.expireTime = !!that.expireTime ? that.expireTime : parseInt(data[1])
+        that.refreshToken = !!that.refreshToken ? that.refreshToken : data[2]
+        that.refreshTokenTimeout = !!that.refreshTokenTimeout ? that.refreshTokenTimeout : data[3]
+      })
+  }
+  init(this)
 }
 
-module.exports.prototype.aopSignature = function(params, needPrefix) {
-  var urlPath = "param2/1/system/currentTime/" + this.clientKey,
+module.exports.prototype.aopSignature = function(params, url) {
+  var urlPath = "",
       _sub = ""
   if(params){
     _sub = Object.keys(params).map(function(k) { return (k + "" + params[k]) }).sort().join("")
   }
-  if(!needPrefix){
-    urlPath = ""
+  if(url && url.indexOf("param2")!= -1){
+    urlPath = url.slice(url.indexOf("param2"), url.length)
   }
   sha1Str = crypto.createHmac('sha1', this.clientSecret).update(urlPath+_sub).digest('hex')
   return sha1Str.toUpperCase()
 }
 
-module.exports.prototype.genAuthUri = function(args){
+module.exports.prototype.genAuthUri = function(redirect_uri, state){
   //http://gw.api.alibaba.com/auth/authorize.htm?client_id=xxx&site=aliexpress&redirect_uri=YOUR_REDIRECT_URL&state=YOUR_PARM&_aop_signature=SIGENATURE
   var url = "http://gw.api.alibaba.com/auth/authorize.htm",
   params = {
     client_id: this.clientKey,
     site: "aliexpress",
-    redirect_uri: args["redirect_uri"]
+    redirect_uri: redirect_uri
   }
-  if(args["state"]){
-    params["state"] = args["state"]
+  if(state){
+    params["state"] = state
   }
   params["_aop_signature"] = this.aopSignature(params)
   console.log(params)
@@ -40,7 +61,7 @@ module.exports.prototype.genAuthUri = function(args){
 
 
 //https://gw.api.alibaba.com/openapi/http/1/system.oauth2/getToken/YOUR_APPKEY?grant_type=authorization_code&need_refresh_token=true&client_id= YOUR_APPKEY&client_secret= YOUR_APPSECRET&redirect_uri=YOUR_REDIRECT_URI&code=CODE
-module.exports.prototype.getRefreshToken = function(redirect_uri, code){
+module.exports.prototype.getRefreshToken = function(redirect_uri, code, store){
   var that = this,
       uri = "https://gw.api.alibaba.com/openapi/http/1/system.oauth2/getToken/"+that.clientKey,
       params = {
@@ -55,50 +76,140 @@ module.exports.prototype.getRefreshToken = function(redirect_uri, code){
         uri: uri,
         method: "POST",
         form: params
+      },
+      _store = store || function(refreshTokenTimeout, aliId, resourceOwner, expireTime, refreshToken, accessToken){
+        client.hmset(["aliexpress:info", "accessToken", accessToken, "expireTime", ((new Date()).getTime() + expireTime * 1000),
+          "refreshTokenTimeout", refreshTokenTimeout, "aliId", aliId, "resourceOwner", resourceOwner, "refreshToken", refreshToken])
       }
 
-  return _request(options)
+
+  return new Promise(function(resolve, reject){
+    request.post(options, function(err, res, body){
+      console.log(err, res.statusCode, body)
+      if (!err && res.statusCode == 200) {
+        var data = JSON.parse(body)
+        _store(data.refresh_token_timeout, data.aliId, data.resource_owner, data.expires_in, data.refresh_token, data.access_token)
+        resolve(data.resource_owner, data.refresh_token, data.access_token)
+      }else{
+        reject(err)
+      }
+    })
+  })
+
 }
 
 
 // https://gw.api.alibaba.com/openapi/param2/1/system.oauth2/getToken/YOUR_APPKEY
 // 请求参数如下：
 // grant_type=refresh_token&client_id=YOUR_APPKEY&client_secret=YOUR_APPSECRET&refresh_token=REFRESH_TOKEN
-module.exports.prototype.getAccessToken = function(refresh_token, isValid, store){
-  if(isValid && store && isValid()){
-    return new Promise(function(resolve, reject){
-      store(resolve)
-    })
-  }else{
-    var that = this,
-        uri = "https://gw.api.alibaba.com/openapi/param2/1/system.oauth2/getToken/"+that.clientKey,
-        params = {
-          grant_type: "refresh_token",
-          client_id: that.clientKey,
-          client_secret: that.clientSecret,
-          refresh_token: refresh_token
-        },
-        options = {
-          uri: uri,
-          method: "POST",
-          form: params
+module.exports.prototype.refreshAccessToken = function(refresh_token, isValid, store){
+  var that = this,
+      _store = store || function(accessToken, expiresIn){
+        ((new Date()).getTime() + expiresIn * 1000)
+        client.hmset(["aliexpress:info", "accessToken", accessToken, "expireTime", ((new Date()).getTime() + expiresIn * 1000)])
+      }
+      that.isValid = isValid || that.isValid
+      that.refreshToken = refresh_token || that.refreshToken
+
+  return new Promise(function(resolve, reject){
+    if(that.isValid()){
+      resolve(that.accessToken)
+    }else{
+      var uri = "https://gw.api.alibaba.com/openapi/param2/1/system.oauth2/getToken/"+that.clientKey,
+          params = {
+            grant_type: "refresh_token",
+            client_id: that.clientKey,
+            client_secret: that.clientSecret,
+            refresh_token: that.refreshToken
+          },
+          options = {
+            uri: uri,
+            method: "POST",
+            form: params
+          }
+      request.post(options, function(err, res, body){
+        console.log(err, res.statusCode, body)
+        if (!err && res.statusCode == 200) {
+          var data = JSON.parse(body),
+          expiresIn = parseInt(data.expires_in)
+          that.accessToken = data.access_token
+          that.expireTime = ((new Date()).getTime() + expiresIn * 1000)
+          _store(data.access_token, expiresIn)
+          resolve(data.access_token)
+        }else{
+          reject(err)
         }
+      })
+    }
+  })
+}
 
-    return _request(options)
+module.exports.prototype.isValid = function () {
+  return !!this.accessToken && (new Date().getTime()) < this.expireTime;
+};
+
+
+module.exports.prototype.onlineAeProduct = function(productIds){
+  return _onOffProducts.call(this, productIds, "api.onlineAeProduct")
+}
+
+module.exports.prototype.offlineAeProduct = function(productIds){
+  return _onOffProducts.call(this, productIds, "api.offlineAeProduct")
+}
+
+function _onOffProducts(productIds, name){
+  if(productIds.length > 50){
+    throw new Error("out of 50 product ids")
   }
+  var uri = "http://" + this.host + this.suburl + "/" + this.version + "/" + this.namespace + "/" + name + "/" + this.clientKey,
+      that = this
+  return new Promise(function(resolve, reject){
+    async.waterfall([function(next){
+      that.refreshAccessToken(that.refreshToken).then(function(accessToken){
+        next(null, accessToken)
+      }).catch(function(err){
+        reject(err)
+      })
+    }, function(accessToken){
+      var params = {
+        _aop_timestamp: Date.now(),
+        access_token: accessToken,
+        productIds: productIds.join(";")
+      }
+      params["_aop_signature"] = that.aopSignature(params, uri)
+      var options = {
+        uri: uri,
+        method: "POST",
+        form: params
+      }
+      console.log(options)
+      _requestPost.call(that, options).then(function(data){
+        if(data.error_code){
+          reject(new Error(data.error_code + ":" + data.error_message))
+        }else{
+          resolve(data.success)
+        }
+      }).catch(function(err){
+        reject(err)
+      })
+    }], function(err){
+      if(err){
+        console.log(err)
+        reject(err)
+      }
+    })
+  })
 }
 
-function _getAccessToken(){
 
-}
-
-
-function _request(options){
+function _requestPost(options){
+  var that = this
   return new Promise(function(resolve, reject){
     request.post(options, function(err, res, body){
       console.log(err, res.statusCode, body)
       if (!err && res.statusCode == 200) {
-        resolve(body)
+        var data = JSON.parse(body)
+        resolve(data)
       }else{
         reject(err)
       }
